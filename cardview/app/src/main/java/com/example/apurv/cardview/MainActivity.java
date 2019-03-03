5caf8639dbe939cb00472ee05f8e10b15b7d02c8
Apurv <a.sharma@thesynapses.com>
2019-03-03 14:51:38 +0530

    yo yo

---
+++ cardview/app/src/main/java/com/example/apurv/cardview/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.apurv.cardview;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    //a list to store all the products
+    List<product> productList;
+
+    //the recyclerview
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //getting the recyclerview from xml
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //initializing the productlist
+        productList = new ArrayList<>();
+
+
+        //adding some items to our list
+        productList.add(
+                new product(
+                        1,
+                        "Apple MacBook Air Core i5 5th Gen - (8 GB/128 GB SSD/Mac OS Sierra)",
+                        "13.3 inch, Silver, 1.35 kg",
+                        4.3,
+                        60000,
+                        R.drawable.ppp));
+
+        productList.add(
+                new product(
+                        1,
+                        "Dell Inspiron 7000 Core i5 7th Gen - (8 GB/1 TB HDD/Windows 10 Home)",
+                        "14 inch, Gray, 1.659 kg",
+                        4.3,
+                        60000,
+                        R.drawable.qq1));
+
+        productList.add(
+                new product(
+                        1,
+                        "Microsoft Surface Pro 4 Core m3 6th Gen - (4 GB/128 GB SSD/Windows 10)",
+                        "13.3 inch, Silver, 1.35 kg",
+                        4.3,
+                        60000,
+                        R.drawable.rrr));
+
+        //creating recyclerview adapter
+        productadapter adapter = new productadapter(this, productList);
+
+        //setting adapter to recyclerview
+        recyclerView.setAdapter(adapter);
+    }
+}
